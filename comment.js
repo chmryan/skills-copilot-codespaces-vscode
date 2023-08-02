ced06429700a825aaf41634a22860192b1d831f2
Chris Ryan <18198524+dappergibbon@users.noreply.github.com>
2023-08-02 20:14:21 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,98 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// Path: comment.js
+// Create web server
+const
